226ed38aecb7836dd5f854c220fcd839ea751970
gavin <106952090+gengzhiyao@users.noreply.github.com>
2025-11-27 20:57:10 +0800

    add

--- StrategyPattern/Strategy.cpp
+++ StrategyPattern/Strategy.cpp
@@ -5,7 +5,6 @@ void Strategy::execute()
     std::cout << "Strategy::execute" << std::endl;
 }
 
-<<<<<<< HEAD
 void ConcreateStrategyAdd::execute()
 {
     std::cout << "ConcreateStrategyAdd::execute" << std::endl;
@@ -15,5 +14,3 @@ void ConcreateStrategySub::execute()
 {
     std::cout << "ConcreateStrategySub::execute" << std::endl;
 }
-=======
->>>>>>> 0628f2ed7b96c2d1b72240f45a5680f79de5c821
